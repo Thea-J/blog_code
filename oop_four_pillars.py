6432e0caba0431acd3b333409764f9a51bb0e9c3
github username <63208817+Thea-J@users.noreply.github.com>
2021-02-11 23:28:32 +0100

    encapsulation and polymorphism examples part complete

--- oop_four_pillars.py
+++ oop_four_pillars.py
@@ -7,27 +7,29 @@ class SoftwarePioneer:
 
     # Initialise each new instance of the SoftwarePioneer class
     # New objects will be assigned these attributes upon creation
-    def __init__(self, fullName, profession, creation, dateCreated, dateRecognised, recognitionBody):
-        self.fullName = fullName
+    def __init__(self, full_name, profession, creation, date_created, date_recognised, recognition_body):
+        self.full_name = full_name
         self.profession = profession
         self.creation = creation
-        self.dateCreated = dateCreated
-        self.dateRecognised = dateRecognised
-        self.recognitionBody = recognitionBody
+        self.date_created = date_created
+        self.date_recognised = date_recognised
+        self.recognition_body = recognition_body
 
-    # Define the Instance method changeTheWorld that returns a string
-    def changeTheWorld(self):
-        print(f"{self.fullName} changed the world in {self.dateCreated} with the creation of the {self.creation}");
+    # Define the Instance method change_the_world that returns a string
+    def change_the_world(self):
+        print(f"{self.full_name} changed the world in {self.date_created} with the creation of the {self.creation}");
 
     # Define the Instance method recognition that returns a string
     def recognition(self):
-        print(f"{self.fullName}'s work was recognised in {self.dateRecognised} by The {self.recognitionBody}. This was approximately {self.dateRecognised - self.dateCreated} years after their work pioneered {self.creation}.");
+        print(f"{self.full_name}'s work was recognised in {self.date_recognised} by The {self.recognition_body}. This was approximately {self.date_recognised - self.date_created} years after their work pioneered {self.creation}.");
 
- # Create (Instantiate) a new object from the SoftwarePioneer class and assign to a variable
+# Create (Instantiate) a new object from the SoftwarePioneer class and assign to a variable
 mark = SoftwarePioneer("Mark E. Dean", "Computer Scientist and engineer", "Coloured Computer Monitor", 1980, 1997, "National Inventors Hall of Fame");
-Marian = SoftwarePioneer("Marian Croak", "Computer Scientist and engineer", "Voice Over Internet Protocol (VoIP)", 1995, 2013, "Women in Technology International hall of fame");
-mark.changeTheWorld();
-Marian.recognition();
+marian = SoftwarePioneer("Marian Croak", "Computer Scientist and engineer", "Voice Over Internet Protocol (VoIP)", 1995, 2013, "Women in Technology International hall of fame");
+
+#Call methods and variables
+mark.change_the_world();
+marian.recognition();
 print(mark.expertise);
     
 
@@ -39,8 +41,26 @@ class HardwarePioneer(SoftwarePioneer):
     # The class attribute expertise has been overridden for the child class
     expertise = "Hardware";
 
+    # Define a private variable age that can only be accessed within the method setage
+    __age = 0;
+
+    # Define a public method setage that updates the private variable __age
+    def setage(self, age):
+        self.__age = age
+    
+    # Define a public method getage that retrieves the private variable __age
+    def getage(self):
+        return self.__age
+
+    # Define the Instance method change_the_world that returns a string
+    def change_the_world(self):
+        print(f"{self.full_name} changed the world in {self.date_created}, at the age of {self.getage()} with the creation of the {self.creation}");
+
 # Create (Instantiate) a new object from the HardwarePioneer class and assign to a variable
-mary = HardwarePioneer("Mary Van Brittan Brown", "Computer Scientist and engineer", "Home Security System", 1966, 1997, "National Inventors Hall of Fame");
-mary.changeTheWorld();
-print(mary.expertise);
-print(isinstance(mary, SoftwarePioneer)); 
+marie = HardwarePioneer("Marie Van Brittan Brown", "Computer Scientist and engineer", "Home Security System", 1966, 1969, "National Scientists Committee");
+
+#Call methods and variables
+print(marie.expertise);
+marie.setage(21);
+marie.change_the_world();
+print(isinstance(marie, SoftwarePioneer)); #All objects created from a child class are instances of the parent class.
