b9e32312a8e1858d5cb06e770eff747f816bfa90
github username <63208817+Thea-J@users.noreply.github.com>
2021-02-16 15:10:29 +0100

    done/

--- oop_four_pillars.py
+++ oop_four_pillars.py
@@ -17,19 +17,19 @@ class SoftwarePioneer:
 
     # Define the Instance method change_the_world that returns a string
     def change_the_world(self):
-        print(f"{self.full_name} changed the world in {self.date_created} with the creation of the {self.creation}");
+        return f"{self.full_name} changed the world in {self.date_created} with the creation of the {self.creation}";
 
     # Define the Instance method recognition that returns a string
     def recognition(self):
-        print(f"{self.full_name}'s work was recognised in {self.date_recognised} by The {self.recognition_body}. This was approximately {self.date_recognised - self.date_created} years after their work pioneered {self.creation}.");
+        return f"{self.full_name}'s work was recognised in {self.date_recognised} by The {self.recognition_body}. This was approximately {self.date_recognised - self.date_created} years after their work pioneered {self.creation}.";
 
 # Create (Instantiate) a new object from the SoftwarePioneer class and assign to a variable
 mark = SoftwarePioneer("Mark E. Dean", "Computer Scientist and engineer", "Coloured Computer Monitor", 1980, 1997, "National Inventors Hall of Fame");
 marian = SoftwarePioneer("Marian Croak", "Computer Scientist and engineer", "Voice Over Internet Protocol (VoIP)", 1995, 2013, "Women in Technology International hall of fame");
 
 #Call methods and variables
-mark.change_the_world();
-marian.recognition();
+print(mark.change_the_world());
+print(marian.recognition());
 print(mark.expertise);
     
 
@@ -42,25 +42,28 @@ class HardwarePioneer(SoftwarePioneer):
     expertise = "Hardware";
 
     # Define a private variable age that can only be accessed within the method setage
-    __age = 0;
+    __age_at_creation = None;
 
     # Define a public method setage that updates the private variable __age
     def setage(self, age):
-        self.__age = age
+        self.__age_at_creation = age
     
     # Define a public method getage that retrieves the private variable __age
     def getage(self):
-        return self.__age
+        return self.__age_at_creation
 
     # Define the Instance method change_the_world that returns a string
     def change_the_world(self):
         print(f"{self.full_name} changed the world in {self.date_created}, at the age of {self.getage()} with the creation of the {self.creation}");
+
 
 # Create (Instantiate) a new object from the HardwarePioneer class and assign to a variable
 marie = HardwarePioneer("Marie Van Brittan Brown", "Computer Scientist and engineer", "Home Security System", 1966, 1969, "National Scientists Committee");
 
 #Call methods and variables
 print(marie.expertise);
-marie.setage(21);
-marie.change_the_world();
+marie.setage(44);
+print(marie.recognition());
+print(marie.change_the_world());
 print(isinstance(marie, SoftwarePioneer)); #All objects created from a child class are instances of the parent class.
+# print(marie.__age_at_creation)
